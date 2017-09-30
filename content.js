0c0bee69fa34f87ffc56151a2e257cff756d7138
Robin Yuan <ryuan01@gmail,com>
2017-09-30 15:58:44 -0700

    right click is working

---
+++ content.js
@@ -0,0 +1,20 @@
+// content.js
+
+//add right click function
+// $(document).ready(function(){
+// 	$("p").contextmenu(function(){
+// 		alert("Hello World!");
+// 	})
+// });
+
+// function getword(info,tab) {
+//   console.log("Word " + info.selectionText + " was clicked.");
+//   chrome.tabs.create({  
+//     url: "http://www.google.com/search?q=" + info.selectionText,
+//   });           
+// }
+// chrome.contextMenus.create({
+//   title: "Search: %s", 
+//   contexts:["selection"], 
+//   onclick: getword,
+// });
