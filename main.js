f1f76948cc07ab1be9bc3289548e4a2bd7993c99
Hannah Lin <hxqlin@gmail.com>
2017-10-01 11:58:53 -0700

    added get request for set IDs

--- main.js
+++ main.js
@@ -17,3 +17,17 @@ chrome.runtime.onMessage.addListener(
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+getSets(function () {
+    var username = "hannerzer";
+    var url = String.concat('https://api.quizlet.com/2.0/users/', username, 'sets');
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        var setIDs = [];
+        for (var object in data) {
+            var setID = object["id"];
+            setIDs.push(setID);
+        }
+    });
+});
