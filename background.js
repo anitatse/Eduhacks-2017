932351d5bba16d29066e6c9d84321c0fc56abd75
Hannah Lin <hxqlin@gmail.com>
2017-09-30 19:22:43 -0700

    can receive response from Quizlet API

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // Set up context menu at install time.
 chrome.runtime.onInstalled.addListener(function() {
-  var context = "selection";
-  var title = "Add selected text to Flash cards";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});  
+    var context = "selection";
+    var title = "Create a flash card";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+        "id": "context" + context});
 });
 
 // add click event
@@ -11,14 +11,21 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 // The onClicked callback function.
 function onClickHandler(info, tab) {
-  var sText = info.selectionText;
+    // var sText = info.selectionText;
 
-  //change the rest to put into the flashcard deck
+    //change the rest to put into the flashcard deck
 
-  //open a pop-up to edit the card.
-  var flashCard = chrome.windows.create({"type": "popup"});
+    //open a pop-up to edit the card.
+    //var flashCard = chrome.windows.create({"type": "popup"});
+    alert('clicked!');
 
-  //add the card to the deck 
-  // var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);  
-  // window.open(url, '_blank');
-};
+    fetch('https://api.quizlet.com/2.0/users/hannerzer?access_token=aVWG3KmyyTNPQAgVyWgXFsdPHFXxFWM6SPsyBE9m&whitespace=1').then(function(response) {
+        return response.json();
+    }).then(function(data) {
+      var modified = JSON.stringify(data);
+        alert(modified);
+    });
+    //add the card to the deck
+    // var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);
+    // window.open(url, '_blank');
+}
