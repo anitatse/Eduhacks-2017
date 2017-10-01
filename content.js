3dbbc06f0fec63e4ebd85ed716688ab0cc4fc078
Robin Yuan <ryuan01@gmail,com>
2017-10-01 09:55:05 -0700

    with input field for definition

--- content.js
+++ content.js
@@ -71,8 +71,17 @@ function addCard(word, def){
 
 	var body = document.createElement("div");
 	body.setAttribute("class", "modal-body");
-	var definition = document.createElement("p");
-	definition.innerHTML = def;
+	// var definition = document.createElement("p");
+	// definition.innerHTML = def;
+	var definition = document.createElement("div");
+	definition.setAttribute("class","form-group");
+	var label = document.createElement("label");
+	label.setAttribute("for","comment");
+	label.innerHTML = "Definition: ";
+	var textArea = document.createElement("textarea");
+	textArea.setAttribute("class","form-control");
+	textArea.setAttribute("rows", "5");
+	textArea.setAttribute("id","definition");
 	var footer = document.createElement("div");
 	footer.setAttribute("class", "modal-footer");
 	var btnAdd = document.createElement("button");
@@ -85,6 +94,9 @@ function addCard(word, def){
 	btnCancel.setAttribute("class", "btn btn-link");
 	btnCancel.setAttribute("data-dismiss", "modal");
 	btnCancel.innerHTML = "Cancel";
+
+	definition.appendChild(label);
+	definition.appendChild(textArea);
 
 	footer.appendChild(btnAdd);
 	footer.appendChild(btnCancel);
@@ -99,7 +111,7 @@ function addCard(word, def){
 	dialog.appendChild(content);
 	modal.appendChild(dialog);
 
-	btnAdd.addEventListener("click", function() { saveCard(word, def);});
+	btnAdd.addEventListener("click", function() { saveCard(word, textArea.value);});
 
 	$(modal).modal();
 
@@ -111,99 +123,3 @@ function saveCard(word, definition){
 	console.log(word);
 	console.log(definition);
 }
-
-
-function createModalTrigger(){
-	var button = document.createElement("button");
-	button.type="button";
-	button.class = "btn btn-info btn-lg";
-	button.setAttribute("data-toggle","modal");
-	button.setAttribute("data-target","#myModal");
-	button.appendChild(document.createTextNode("open modal!"));
-	return button;
-	// var p = document.createElement("p");
-	// var a = document.createElement("a");
-	// a.href="#ex1";
-	// a.rel="modal:open";
-	// a.appendChild(document.createTextNode("Open Modal!"));
-	// p.appendChild(a);
-	// return p;
-}
-
-function createModal(word){
-	var div = document.createElement("div");
-	div.class = "modal fade";
-	div.id = "myModal";
-	div.role = "dialog";
-
-	var divDialog = document.createElement("div");
-	divDialog.class = "modal-dialog";
-
-	var divContent = document.createElement("div");
-	divContent.class = "modal-content";
-
-	var divHeader = document.createElement("div");
-	divHeader.class = "modal-header";
-
-	var buttonClose = document.createElement("button");
-	buttonClose.type = "button";
-	buttonClose.class = "close";
-	buttonClose.setAttribute("data-dismiss","modal");
-	buttonClose.appendChild(document.createTextNode("x"));
-
-	var h4 = document.createElement("h4");
-	h4.class = "modal-title";
-	h4.appendChild(document.createTextNode(word));
-
-	divHeader.appendChild(buttonClose);
-	divHeader.appendChild(h4);
-
-	var divBody = document.createElement("div");
-	divBody.class = "modal-body";
-
-	var p = document.createElement("p");
-	p.appendChild(document.createTextNode("Some text in the modal."));
-
-	divBody.appendChild(p);
-
-	var divFooter = document.createElement("div");
-	divFooter.class = "modal-footer";
-
-	var buttonAddCard = document.createElement("button");
-	buttonAddCard.type = "button";
-	buttonAddCard.class = "btn btn-default";
-	// buttonAddCard.setAttribute("data-dismiss", "modal");
-	buttonAddCard.appendChild(document.createTextNode("add card"));
-
-	divFooter.appendChild(buttonAddCard);
-
-	divContent.appendChild(divHeader);
-	divContent.appendChild(divBody);
-	divContent.appendChild(divFooter);
-
-	divDialog.appendChild(divContent);
-
-	div.appendChild(divDialog);
-
-	return div;
-}
-// <!-- Modal -->
-//   <div class="modal fade" id="myModal" role="dialog">
-//     <div class="modal-dialog">
-    
-//       <!-- Modal content-->
-//       <div class="modal-content">
-//         <div class="modal-header">
-//           <button type="button" class="close" data-dismiss="modal">&times;</button>
-//           <h4 class="modal-title">word</h4>
-//         </div>
-//         <div class="modal-body">
-//           //ignoring definition right now
-//           <button>add card</button>
-//         </div>
-//         <div class="modal-footer">
-//           <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-//         </div>
-//       </div>
-//     </div>
-//   </div>
